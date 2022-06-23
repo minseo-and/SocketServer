25ab297c3a118acdde615c99421381ad2d460eed
minseo <msp05070478@gmail.com>
2022-06-23 12:07:37 +0900

    접속 포트 구현

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -1,4 +1,12 @@
+import java.net.ServerSocket
 
 fun main(){
+    //서버 역할용 객체 생성
+    val server = ServerSocket(8080)
+    println("사용자 접속 대기")
+    val socket = server.accept()
+    println(socket)
 
+    //서버 종료
+    socket.close()
 }
