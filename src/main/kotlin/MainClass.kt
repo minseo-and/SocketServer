bcadfe749ac596dd1ecf851a6ebe3d5db52e1525
minseo <msp05070478@gmail.com>
2022-06-23 13:33:57 +0900

    클라이언트로 메시지 전달

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -1,3 +1,4 @@
+import java.io.DataOutputStream
 import java.net.ServerSocket
 
 fun main(){
@@ -6,6 +7,15 @@ fun main(){
     println("사용자 접속 대기")
     val socket = server.accept()
     println(socket)
+
+    //클라이언트로 데이터 보내기
+    val outPutString = socket.getOutputStream()
+    val dos = DataOutputStream(outPutString)
+
+    dos.writeInt(100)
+    dos.writeDouble(11.11)
+    dos.writeBoolean(true)
+    dos.writeUTF("서버가 보낸 문자열")
 
     //서버 종료
     socket.close()
